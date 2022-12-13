4c3892541e38bd39a19c3b6988a2a68523191ff6
Dular99 <117625582+OshanDulara@users.noreply.github.com>
2022-12-13 21:59:25 +0530

    calculator uploaded

---
+++ Calculator3/src/main/java/campus/calculator3/Calculator3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package campus.calculator3;
+
+/**
+ *
+ * @author Oshan Dulara
+ */
+public class Calculator3 {
+
+    public static void main(String[] args) {
+        CAL3 cal3=new CAL3();
+        cal3.setVisible(true);
+    }
+}
